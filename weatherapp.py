b8a2954bb3688668329caabcb4c11417f2015d89
BaaqsSi <alahomsa9@gmail.com>
2024-05-26 18:13:49 +0400

    Update weatherapp.py

--- weatherapp.py
+++ weatherapp.py
@@ -2,12 +2,15 @@ import sqlite3
 import requests
 import json
 
+# Access key  weatherstack APIs
 access_key = "91950bcdba3256bdfa3df833bbaa1e11"
 url = f"http://api.weatherstack.com/current?access_key={access_key}"
 
-location = input("Enter your desired location: ")
+#user -- is inputi lokaciis gasagebad
+location = input("chawere sasurveli lokacia:  ")
 
-querystring = {"query": location}
+querystring = {"query": location}#weatherstackis documentaciidan gadmotanili ---
+
 
 response = requests.get(url, params=querystring)
 
@@ -15,30 +18,24 @@ if response.status_code == 200:
     data = response.json()
 
     if "current" in data:
-        # Connect to the SQLite database
         conn = sqlite3.connect('weather_data.db')
         c = conn.cursor()
 
-        # Create a table to store weather information if not exists
+        #table informaciis dasastorad
         c.execute('''CREATE TABLE IF NOT EXISTS weather (
-                     id INTEGER PRIMARY KEY,
-                     location TEXT,
-                     temperature REAL,
-                     weather_description TEXT
+                     location TEXT,          -- lokaciis saxeli
+                     temperature REAL,       -- Temperature in Celsius
+                     weather_description TEXT -- weatheris description
                      )''')
-
-        # Insert data into the database
         c.execute("INSERT INTO weather (location, temperature, weather_description) VALUES (?, ?, ?)",
                   (location, data["current"].get("temperature"), data["current"].get("weather_descriptions", ["N/A"])[0]))
 
-        # Commit changes and close connection
         conn.commit()
         conn.close()
-
         print("Weather information for", location)
-        print("Temperature:", data["current"].get("temperature"), "°C")
+        print("Temperature:", data["current"].get("temperature"), "°C")#esec weatherstackis documentaciidan gadmotanili--
         print("Weather Description:", data["current"].get("weather_descriptions", ["N/A"])[0])
     else:
-        print("No current weather information available.")
+        print("weatheris info araris xelmisawvdomi (albat)")
 else:
-    print("Failed to fetch weather data. Status code:", response.status_code)
+    print("weather data ver aigo:", response.status_code)
